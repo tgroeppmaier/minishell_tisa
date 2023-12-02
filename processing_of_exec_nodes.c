13c9282890f5631f45104abc0043dbbfecf050d5
tgroeppm <tgroeppm@student.42.fr>
2023-12-02 17:54:17 +0100

    added quote expansion. more tests needed

--- processing_of_exec_nodes.c
+++ processing_of_exec_nodes.c
@@ -1,16 +1,151 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   processing_of_exec_nodes.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aminakov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tgroeppm <tgroeppm@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 22:02:27 by aminakov          #+#    #+#             */
-/*   Updated: 2023/11/23 17:26:26 by aminakov         ###   ########.fr       */
+/*   Created: 2023/11/19 22:02:27 by Ektin Op Ur       #+#    #+#             */
+/*   Updated: 2023/12/01 21:09:04 by tgroeppm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "string.h"
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char	*str;
+
+	str = malloc(n + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, n + 1);
+	return (str);
+}
+
+// char	*remove_outer_quotes(char *str)
+// {
+// 	int		pos_open;
+// 	int		pos_close;
+// 	char	quote;
+// 	char	*tmp1;
+// 	char	*tmp2;
+// 	t_sgm	sgm;
+// 	char	*new_str;
+// 	int 	i = 0;
+
+// 	sgm.e = (int)ft_strlen(str);
+// 	sgm.b = 0;
+// 	if (sgm.e < 2 || -1 == get_quote_pos(str, sgm))
+// 		return (ft_strdup(str));
+// 	pos_open = get_quote_pos(str, sgm);
+// 	if (pos_open != -1)
+// 	{
+// 		quote = str[pos_open];
+// 		pos_close = find_symb(str, quote, cr_sgm(pos_open + 1, sgm.e));
+// 		if(pos_close == pos_open + 1)
+// 		{
+// 			// ft_printf("test:%sword", ft_strdup(""));
+// 			return(ft_strdup(""));
+// 		}
+// 	}
+// 	new_str = ft_strdup("");
+// 	while(i < sgm.e)
+// 	{
+// 		sgm.b = i;
+// 		pos_open = get_quote_pos(str, sgm);
+// 		ft_printf("open: %d\n", pos_open);
+// 		if (-1 == pos_open)
+// 		{
+// 			tmp1 = ft_strdup(str + i);
+// 			tmp2 = ft_strjoin(new_str, tmp1);
+// 			free(new_str);
+// 			new_str = tmp2;
+// 			free(tmp1);
+// 			return (new_str);
+// 		}
+// 		quote = str[pos_open];
+// 		pos_close = find_symb(str, quote, cr_sgm(pos_open + 1, sgm.e));
+// 		ft_printf("close: %d\n", pos_close);
+// 		free(new_str);
+// 		new_str = ft_strndup(str, pos_open);
+// 		tmp1 = ft_strndup(str + pos_open + 1, pos_close - pos_open - 1);
+// 		tmp2 = new_str;
+// 		new_str = ft_strjoin(new_str, tmp1);
+// 		free(tmp1);
+// 		free(tmp2);
+// 		i = pos_close + 1;
+// 	}
+// 	return(new_str);
+// }
+
+char *remove_outer_quotes(char *str) 
+{
+    int pos_open, pos_close;
+    char quote;
+    char *tmp1;
+    t_sgm sgm;
+    char *new_str;
+    int i = 0;
+
+	new_str = ft_strdup("");
+    sgm.e = (int)ft_strlen(str);
+    if (sgm.e < 2) 
+	{
+        free(new_str);
+        return (ft_strdup(str));
+    }
+
+    while (i < sgm.e) 
+	{
+        sgm.b = i;
+        pos_open = get_quote_pos(str, sgm);
+        if (-1 == pos_open) 
+		{
+            tmp1 = ft_strdup(str + i);
+            char *tmp2 = new_str;
+            new_str = ft_strjoin(new_str, tmp1);
+            free(tmp1);
+            free(tmp2);
+            break;
+        }
+        quote = str[pos_open];
+        pos_close = find_symb(str, quote, cr_sgm(pos_open + 1, sgm.e));
+        tmp1 = ft_strndup(str + i, pos_open - i);
+        char *tmp2 = new_str;
+        new_str = ft_strjoin(new_str, tmp1);
+        free(tmp1);
+        free(tmp2);
+
+        tmp1 = ft_strndup(str + pos_open + 1, pos_close - pos_open - 1);
+        tmp2 = new_str;
+        new_str = ft_strjoin(new_str, tmp1);
+        free(tmp1);
+        free(tmp2);
+
+        i = pos_close + 1;
+    }
+    return new_str;
+}
+
+void expand_quotes(t_tree *tree)
+{
+	t_list *current;
+	char *tmp;
+	
+	current = tree->list;
+	while(current)
+	{
+		tmp = ft_strdup(current->word);
+		free(current->word);
+		current->word = remove_outer_quotes(tmp);
+		// ft_printf("%s\n", current->word);    // for testing
+		current = current->next;
+		free(tmp);
+	}
+}
 
 int	expand_exec_nodes(t_tree *tree)
 {
@@ -31,5 +166,6 @@ int	expand_exec_nodes(t_tree *tree)
 	}
 	expand_exec_nodes(tree->left_child);
 	expand_exec_nodes(tree->right_child);
+	expand_quotes(tree);
 	return (0);
 }
