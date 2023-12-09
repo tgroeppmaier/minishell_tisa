5b2f64d94752eec42a12bb28416960055dc69355
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2023-12-09 12:00:45 +0100

    Add signal handling for SIGINT

--- main_builtin_test.c
+++ main_builtin_test.c
@@ -9,6 +9,16 @@
 
 volatile sig_atomic_t sigint_received = 0;
 
+void handle_sigint(int sig) 
+{
+	(void)sig;
+    sigint_received = 1;
+    write(STDOUT_FILENO, "\n", 1); // Safely write a newline character
+    rl_on_new_line(); // Prepare readline to handle the new line
+    rl_replace_line("", 0); // Clear the current line
+    rl_redisplay(); // Redisplay the prompt
+}
+
 void	exec_int_function(char *input, t_data *data)
 {
 	if(ft_strcmp(data->cmd->args[0], "exit") == 0)
@@ -38,15 +48,34 @@ int main()
 	t_command	cmd;
 	extern char **environ;
 
+	signal(SIGINT, handle_sigint);
+	
 	data.envp = copy_environ(environ, 0);
 	data.cmd = &cmd;
 	while (1)
 	{
-		input = readline("run_built_in_commands $ ");
-		if (input[0] == '\0')
+		if (sigint_received) 
 		{
-			free(input);
-			continue ;
+            printf("\nSignal received\n");
+            sigint_received = 0;
+        }
+
+		input = readline("run_built_in_commands $ ");
+		if(input == NULL)
+		{
+			if(sigint_received)
+				continue;
+			break;
+		}
+		// if (input[0] == '\0')
+		// {
+		// 	free(input);
+		// 	continue ;
+		// }
+		if(sigint_received == 1)
+		{
+			write(1, "signal\n", 8);
+			sigint_received = 0;
 		}
 		add_history(input);
 		data.cmd->args = ft_split(input, ' ');
