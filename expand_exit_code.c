8756e5281aa0fcf058c53f92822a72cf492d5808
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2023-12-12 12:54:08 +0100

    update expand_exit_code function

--- expand_exit_code.c
+++ expand_exit_code.c
@@ -6,7 +6,7 @@
 /*   By: tgroeppm <tgroeppm@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 21:48:15 by tgroeppm          #+#    #+#             */
-/*   Updated: 2023/12/12 12:46:26 by tgroeppm         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:53:31 by tgroeppm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,36 +43,75 @@ void	create_new_str(CreateNewStrArgs args, int i)
 	create_new_str(args, i + 1);
 }
 
+// char	*expand_exit_code(const char *str, char *exit_code_str, int i)
+// {
+// 	char	*rest;
+// 	size_t	exit_code_str_len;
+// 	size_t	rest_len;
+// 	char	*result;
+
+// 	if (!str[i])
+// 		return (ft_strdup(""));
+// 	if (str[i] == '$' && str[i + 1] == '?' && !in_single_quotes(str, i))
+// 	{
+// 		rest = expand_exit_code(str, exit_code_str, i + 2);
+// 		exit_code_str_len = ft_strlen(exit_code_str);
+// 		rest_len = ft_strlen(rest);
+// 		result = malloc(exit_code_str_len + rest_len + 1);
+// 		ft_strlcpy(result, exit_code_str, exit_code_str_len + 1);
+// 		ft_strlcat(result, rest, exit_code_str_len + rest_len + 1);
+// 		free(rest);
+// 		return (result);
+// 	}
+// 	else
+// 	{
+// 		rest = expand_exit_code(str, exit_code_str, i + 1);
+// 		rest_len = ft_strlen(rest);
+// 		result = malloc(rest_len + 2);
+// 		result[0] = str[i];
+// 		ft_strlcpy(result + 1, rest, rest_len + 1);
+// 		free(rest);
+// 		return (result);
+// 	}
+// }
+
+char	*expand_exit_code_rest(const char *str, char *exit_code_str, int i)
+{
+    char	*rest;
+    size_t	rest_len;
+    char	*result;
+
+    rest = expand_exit_code(str, exit_code_str, i + 1);
+    rest_len = ft_strlen(rest);
+    result = malloc(rest_len + 2);
+    result[0] = str[i];
+    ft_strlcpy(result + 1, rest, rest_len + 1);
+    free(rest);
+    return (result);
+}
+
 char	*expand_exit_code(const char *str, char *exit_code_str, int i)
 {
-	char	*rest;
-	size_t	exit_code_str_len;
-	size_t	rest_len;
-	char	*result;
+    char	*rest;
+    size_t	exit_code_str_len;
+    size_t	rest_len;
+    char	*result;
 
-	if (!str[i])
-		return (ft_strdup(""));
-	if (str[i] == '$' && str[i + 1] == '?' && !in_single_quotes(str, i))
-	{
-		rest = expand_exit_code(str, exit_code_str, i + 2);
-		exit_code_str_len = ft_strlen(exit_code_str);
-		rest_len = ft_strlen(rest);
-		result = malloc(exit_code_str_len + rest_len + 1);
-		ft_strlcpy(result, exit_code_str, exit_code_str_len + 1);
-		ft_strlcat(result, rest, exit_code_str_len + rest_len + 1);
-		free(rest);
-		return (result);
-	}
-	else
-	{
-		rest = expand_exit_code(str, exit_code_str, i + 1);
-		rest_len = ft_strlen(rest);
-		result = malloc(rest_len + 2);
-		result[0] = str[i];
-		ft_strlcpy(result + 1, rest, rest_len + 1);
-		free(rest);
-		return (result);
-	}
+    if (!str[i])
+        return (ft_strdup(""));
+    if (str[i] == '$' && str[i + 1] == '?' && !in_single_quotes(str, i))
+    {
+        rest = expand_exit_code(str, exit_code_str, i + 2);
+        exit_code_str_len = ft_strlen(exit_code_str);
+        rest_len = ft_strlen(rest);
+        result = malloc(exit_code_str_len + rest_len + 1);
+        ft_strlcpy(result, exit_code_str, exit_code_str_len + 1);
+        ft_strlcat(result, rest, exit_code_str_len + rest_len + 1);
+        free(rest);
+        return (result);
+    }
+    else
+        return expand_exit_code_rest(str, exit_code_str, i);
 }
 
 void	expand_exit(t_tree *tree, int exit_code)
