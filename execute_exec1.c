1670cd12d8d9e07a2ce5e63322538cec3000fa46
tgroeppm <tgroeppm@student.42.fr>
2023-12-13 13:07:27 +0100

    fixed $? bug

--- execute_exec1.c
+++ execute_exec1.c
@@ -6,7 +6,7 @@
 /*   By: tgroeppm <tgroeppm@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 19:40:19 by aminakov          #+#    #+#             */
-/*   Updated: 2023/12/12 21:28:31 by tgroeppm         ###   ########.fr       */
+/*   Updated: 2023/12/13 13:06:49 by tgroeppm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,46 @@
 
 // t_tree	*tree_ptr;
 
-/*
-int	ft_exec_exec(t_tree *tree)
-{
-	int	child;
+// int	ft_exec_exec(t_tree *tree)
+// {
+// 	int	child;
 
-	if (NULL == tree || EXEC != tree->node)
-		return (print_error(1, "tr0|ft_exec_exec with not EXEC node"));
-	if (!tree->list && is_str_empty(tree->cmd, cr_sgm(tree->beg, tree->end)))
-		return (print_error(0, "empty string in ft_exec_exec"));
-	if (!tree->list && \
-			!is_str_empty(tree->cmd, cr_sgm(tree->beg, tree->end)))
-		return (print_error(2, "NULL list in ft_exec_exec"));
-	if (0 == tree->do_in_child_process)
-		return (ft_exec_exec_no_new_process(tree));
-	child = fork();
-	if (-1 == child)
-		return (print_error(3, "fork failed ft_exec_exec"));
-	if (0 == child && restore_signals_in_children())
-		do_in_child_exec_exit(tree);
+// 	if (NULL == tree || EXEC != tree->node)
+// 		return (print_error(1, "tr0|ft_exec_exec with not EXEC node"));
+// 	if (!tree->list && is_str_empty(tree->cmd, cr_sgm(tree->beg, tree->end)))
+// 		return (print_error(0, "empty string in ft_exec_exec"));
+// 	if (!tree->list && \
+// 			!is_str_empty(tree->cmd, cr_sgm(tree->beg, tree->end)))
+// 		return (print_error(2, "NULL list in ft_exec_exec"));
+// 	if (0 == tree->do_in_child_process)
+// 		return (ft_exec_exec_no_new_process(tree));
+// 	child = fork();
+// 	if (-1 == child)
+// 		return (print_error(3, "fork failed ft_exec_exec"));
+// 	if (0 == child && restore_signals_in_children())
+// 		do_in_child_exec_exit(tree);
+// 	if (NULL == tree->data)
+// 		print_error(4, "tree->data is NULL in ft_exec_exec");
+// 	tree->data->exit_code = wait_for_all_return_chosen(child);
+// 	return (tree->data->exit_code);
+// }
+
+int	wait_for_child_and_restore_signals(t_tree *tree, int child)
+{
+	struct sigaction	old_sa;
+
 	if (NULL == tree->data)
 		print_error(4, "tree->data is NULL in ft_exec_exec");
 	tree->data->exit_code = wait_for_all_return_chosen(child);
+	old_sa.sa_handler = handle_sigint;
+	sigemptyset(&old_sa.sa_mask);
+	old_sa.sa_flags = 0;
+	if (sigaction(SIGINT, &old_sa, NULL) < 0)
+		return (print_error(6, "sigaction failed in ft_exec_exec"));
 	return (tree->data->exit_code);
 }
- */
 
-int	create_child_and_setup_signals(t_tree *tree)
+int	ft_exec_exec(t_tree *tree)
 {
 	int					child;
 	struct sigaction	sa;
@@ -67,29 +80,6 @@ int	create_child_and_setup_signals(t_tree *tree)
 		if (sigaction(SIGINT, &sa, NULL) < 0)
 			return (print_error(5, "sigaction failed in ft_exec_exec"));
 	}
-	return (child);
-}
-
-int	wait_for_child_and_restore_signals(t_tree *tree, int child)
-{
-	struct sigaction	old_sa;
-
-	if (NULL == tree->data)
-		print_error(4, "tree->data is NULL in ft_exec_exec");
-	tree->data->exit_code = wait_for_all_return_chosen(child);
-	old_sa.sa_handler = handle_sigint;
-	sigemptyset(&old_sa.sa_mask);
-	old_sa.sa_flags = 0;
-	if (sigaction(SIGINT, &old_sa, NULL) < 0)
-		return (print_error(6, "sigaction failed in ft_exec_exec"));
-	return (tree->data->exit_code);
-}
-
-int	ft_exec_exec(t_tree *tree)
-{
-	int	child;
-
-	child = create_child_and_setup_signals(tree);
 	return (wait_for_child_and_restore_signals(tree, child));
 }
 
